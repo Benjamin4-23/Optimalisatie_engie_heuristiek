8b2477bdc9e33c93e989e76caeb35d379fb1738c
ThibaultDekock <thibault.dekock@student.kuleuven.be>
2024-11-14 09:31:24 +0100

    Update Mysolution

--- Engie_Boss_Fight/src/main/java/org/example/search/MySolution.java
+++ Engie_Boss_Fight/src/main/java/org/example/search/MySolution.java
@@ -1,20 +1,32 @@
 package org.example.search;
 
+import org.example.domain.Graph;
 import org.example.search.framework.Solution;
 
 public class MySolution implements Solution {
+    private Graph graph;
+    private double cost;
+    public MySolution() {
+        // this.graph = datareader bla bla
+        // GenerateSolution();
+    }
     @Override
     public double getObjectiveValue() {
-        return 0;
+        return this.cost;
     }
 
     @Override
     public void setObjectiveValue(double value) {
-
+        this.cost = value;
     }
 
     @Override
     public Object clone() {
         return null;
     }
+
+    public void GenerateSolution() {
+        // Generate a random solution
+    }
+
 }
