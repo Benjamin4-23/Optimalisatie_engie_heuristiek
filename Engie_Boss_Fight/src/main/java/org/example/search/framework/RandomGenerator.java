7914e43b2b0958493fa1f03a7f69b5f6f405e6b9
ThibaultDekock <thibault.dekock@student.kuleuven.be>
2024-12-02 11:10:34 +0100

    Added LA & SA

---
+++ Engie_Boss_Fight/src/main/java/org/example/search/framework/RandomGenerator.java
@@ -0,0 +1,8 @@
+package org.example.search.framework;
+
+import java.util.Random;
+
+public class RandomGenerator {
+    public static final Random random  = new Random(1);
+
+}
---
+++ Engie_Boss_Fight/src/main/java/org/example/search/simulatedannealing/SimulatedAnnealing.java
@@ -0,0 +1,54 @@
+package org.example.search.simulatedannealing;
+
+
+import org.example.search.MyObjectiveFunction;
+import org.example.search.MySolution;
+import org.example.search.SwapTwoPaths;
+import org.example.search.framework.*;
+
+public class SimulatedAnnealing extends SearchAlgorithm {
+    private ObjectiveFunction function;
+    private Solution currentSolution;
+    private Solution bestSolution;
+    private double currentResult;
+    private double bestResult;
+
+    public SimulatedAnnealing(String path) {
+        this.function = new MyObjectiveFunction();
+        this.currentSolution = new MySolution(path);
+        bestSolution = currentSolution;
+        bestResult = function.evaluate(bestSolution,null);
+    }
+
+    @Override
+    public double execute(int numberOfIterations) {
+        currentResult = bestResult;
+        Move move = new SwapTwoPaths();
+        for (int i = 0; i <= numberOfIterations; i++) {
+            if (currentResult < bestResult) {
+                bestResult = currentResult;
+                bestSolution = (MySolution)currentSolution.clone();
+                System.out.println(bestResult);
+
+            } else if (Math.exp((bestResult - currentResult) / Math.sqrt(currentResult) / (1.0001 - ((i * 1.0) / numberOfIterations))) < RandomGenerator.random.nextDouble()) {
+                move.undoMove(currentSolution);
+            }
+            currentResult = function.evaluate(currentSolution, move);
+            //System.out.println(currentResult);
+        }
+        //System.out.println("bestSolution " + function.evaluate(bestSolution,null) + " " + bestResult);
+        return bestResult;
+    }
+
+
+    @Override
+    public Solution getBestSolution() {
+        return bestSolution;
+    }
+
+
+    @Override
+    public Solution getCurrentSolution() {
+        return currentSolution;
+    }
+}
