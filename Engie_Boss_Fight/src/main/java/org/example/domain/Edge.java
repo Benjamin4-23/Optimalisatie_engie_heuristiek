ab57bef2278021bd1577c6f8b42b1cafec3a9e78
Benjamin Rübenkamp <benjamin.ruebenkamp@student.kuleuven.be>
2024-12-02 15:56:32 +0100

    calculate which edges are replaceable

--- Engie_Boss_Fight/src/main/java/org/example/domain/Edge.java
+++ Engie_Boss_Fight/src/main/java/org/example/domain/Edge.java
@@ -14,6 +14,10 @@ public class Edge {
     public Node endNode2;
     public int originalID;
     public List<Edge> oldEdges;
+    public boolean isReplaceable;
+    public int replaceableCost;
+    List<Edge> replacingEdges;
+    public boolean isMoved;
 
     public Edge(Edge edge) {
         this.id = edge.id;
@@ -24,6 +28,10 @@ public class Edge {
         this.endNode2 = edge.endNode2;
         this.isUsed = edge.isUsed;
         this.oldEdges = edge.oldEdges;
+        this.isReplaceable = edge.isReplaceable;
+        this.replaceableCost = edge.replaceableCost;
+        this.replacingEdges = edge.replacingEdges;
+        this.isMoved = false;
     }
 
     public Edge(int id, EdgeType edgeType, int cost, Node endpoint1, Node endpoint2, int originalId) {
@@ -36,6 +44,10 @@ public class Edge {
         this.isUsed = false;
         this.originalID = originalId;
         this.oldEdges = new ArrayList<>();
+        this.isReplaceable = false;
+        this.replaceableCost = 0;
+        this.replacingEdges = new ArrayList<>();
+        this.isMoved = false;
     }
 
     public void Use(){
@@ -52,4 +64,28 @@ public class Edge {
     }
 
     // Getters and setters can be added here if needed
+    public void setReplaceable(boolean replaceable) {
+        isReplaceable = replaceable;
+    }
+    public boolean isReplaceable() {
+        return isReplaceable;
+    }
+    public void setMoved(boolean moved) {
+        isMoved = moved;
+    }
+    public boolean isMoved() {
+        return isMoved;
+    }
+    public void setReplaceableCost(int replaceableCost) {
+        this.replaceableCost = replaceableCost;
+    }
+    public int getReplaceableCost() {
+        return replaceableCost;
+    }
+    public void setReplacingEdges(List<Edge> replacingEdges) {
+        this.replacingEdges = replacingEdges;
+    }
+    public List<Edge> getReplacingEdges() {
+        return replacingEdges;
+    }
 }
