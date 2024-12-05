e3032e80ffc596171e3a1c8109b4101023432be0
Arne Vanmarcke <65821062+ArneVanmarcke@users.noreply.github.com>
2024-12-05 12:18:25 +0100

    Added path

---
+++ Engie_Boss_Fight/src/main/java/org/example/domain/Path.java
@@ -0,0 +1,18 @@
+package org.example.domain;
+
+import java.security.interfaces.EdECKey;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Path<T> {
+    public List<T> edges= new LinkedList<>();
+    public int cost = 0;
+    public void add(int index, T value){
+        edges.add(index, value);
+    }
+    public static Path empty(){
+        return new Path();
+    }
+}
